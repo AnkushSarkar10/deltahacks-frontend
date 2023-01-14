c11ec9e1160d269ba9113217312a8361d0a48059
AnkushSarkar10 <ankush.sarkar2002@gmail.com>
2023-01-14 17:54:50 -0500

    landing page done

--- components/index/third.vue
+++ components/index/third.vue
@@ -4,16 +4,19 @@
 <template>
     <div class="w-100 h-screen bg-primarycol overflow-hidden">
 
-        <div class="flex justify-center text-textcol">
-            
-            <div class="relative top-52 left-5 flex flex-col gap-8 text-6xl font-extrabold text-center">
+        <div class="flex flex-col relative top-40">
+
+            <div class="flex flex-col gap-8 text-6xl font-extrabold text-center text-textcol">
                 <h1>1. Compleate a Course</h1>
                 <h1>2. Get Your NFT Certificate</h1>
                 <h1>3. Share!</h1>
             </div>
             <!-- Make a button in html using tailwind -->
+            <div class="flex justify-center mt-20">
+                <button
+                    class="text-black bg-textcol rounded-full text-white font-extrabold text-2xl w-52 py-3 px-5 drop-shadow-xl uppercase">Sign up</button>
+            </div>
         </div>
-        <button class="text-black bg-textcol rounded-full text-white py-2 px-4">Button</button>
     </div>
 </template>
 
