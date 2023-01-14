8d95ff9d6ac2c2d27135516808d6cf0a63577f1a
Viransh Shah <shahv47@mcmaster.ca>
2023-01-14 16:30:16 -0500

    profile search bar

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,18 +1,14 @@
 <template>
-    <nav class="relative flex flex-wrap items-center justify-between px-2 py-3 bg-pink-500 mb-3 text-textcol">
+    <nav class="relative flex text-3xl flex-wrap items-center justify-between px-2 py-3 bg-pink-500 mb-3 text-textcol">
         <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
             <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
                 <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
                     href="/">
                     Home
                 </a>
-                <button
-                    class="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-                    type="button" v-on:click="toggleNavbar()">
-                    <i class="fas fa-bars"></i>
-                </button>
             </div>
-            <div v-bind:class="{ 'hidden': !showMenu, 'flex': showMenu }" class="lg:flex lg:flex-grow items-center">
+            <input type="text" v-model="input" placeholder="Search Courses" />
+            <div >
                 <ul class="flex flex-col lg:flex-row list-none ml-auto">
                     <li class="nav-item">
                         <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
@@ -35,17 +31,25 @@
 </template>
 
 <script>
-export default {
-    name: "navbar",
-    data() {
-        return {
-            showMenu: false
-        }
-    },
-    methods: {
-        toggleNavbar: function () {
-            this.showMenu = !this.showMenu;
-        }
-    }
+let input = ref("");
+const searchCourses = () => {
+    return fetch(`https://api.coursera.org/api/catalog.v1/courses?q=search&query=${input}`)
+        .then(response => response.json())
+        .then(data => {
+            return data.courses;
+        });
 }
 </script>
+
+<style scoped>
+input {
+    width: 25%;
+    height: 30px;
+    border-bottom: 1px solid white;
+    border-radius: 5px;
+    border-color: white;
+    margin-bottom: 20px;
+    background: transparent;
+    justify-self: center;
+}
+</style>
