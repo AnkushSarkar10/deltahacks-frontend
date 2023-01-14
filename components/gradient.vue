7053571f16c35a604ed9008473b503b369f3bc80
AnkushSarkar10 <ankush.sarkar2002@gmail.com>
2023-01-14 14:54:03 -0500

    gradienntt

---
+++ components/gradient.vue
@@ -0,0 +1,17 @@
+<script setup lang='ts'>
+</script>
+
+<template>
+    <!-- gradient -->
+    <div class="fixed bottom-0 -right-200 h-full w-full gradient"></div>
+    <!--  -->
+</template>
+
+<style scoped>
+.gradient {
+    background: url("public/image.jpg");
+    mix-blend-mode: hard-light;
+    filter: blur(250px);
+    transform: rotate(106.73deg);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,17 +4,9 @@
 <template>
     <div class="w-100 h-screen bg-primarycol">
         <h1 class="text-white text-xl">Sup</h1>
-        <!-- gradient -->
-        <div class="fixed bottom-0 -right-200 h-full w-full gradient"></div>
-        <!--  -->
+        <Gradient />
     </div>
 </template>
 
 <style scoped>
-.gradient {
-    background: url("public/image.jpg");
-    mix-blend-mode: hard-light;
-    filter: blur(250px);
-    transform: rotate(106.73deg);
-}
 </style>
