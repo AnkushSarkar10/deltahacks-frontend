b4031c5ab3751cfd71613b3716e728b47c17832c
Viransh Shah <shahv47@mcmaster.ca>
2023-01-14 14:48:55 -0500

    login and create_account

---
+++ pages/create_account.vue
@@ -0,0 +1,13 @@
+<script setup lang='ts'>
+</script>
+
+<template>
+    <div class="w-100 h-screen bg-primary">
+        <h1>login</h1>
+        <div class="w-100 h-screen blur-xl bg-gradient"></div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
